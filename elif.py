5a9c16115cafd6c511bd7d85c34c1092f4eb28e4
Ryan <ryanblahnik@gmail.com>
2016-04-05 22:43:09 -0500

    Added files via upload
    004

---
+++ elif.py
@@ -0,0 +1,11 @@
+name = 'Bob'
+age = 3000
+if name == 'Alice':
+    print('Hi Alice')
+elif age < 12:
+    print('You are not Alice, kid-o')
+elif age > 2000:
+    print('Unlike you, Alice is not an undead, immortal vampire')
+     print('You aren\'t Alice, imposter')
+elif age > 100:
+    print('You aren\'t Alice, imposter')
