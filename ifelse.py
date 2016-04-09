54f56527d6bd11fbf8596391630185392c9ef660
Ryan <ryanblahnik@gmail.com>
2016-04-08 19:07:32 -0500

    Added files via upload
    006

---
+++ ifelse.py
@@ -0,0 +1,12 @@
+print('Please enter your name.')
+name = input()
+if name !='':
+    print('Thank you, ' + name + '.')
+    print('Enjoy your flight.')
+while name =='':
+        print('You did not enter your name.')
+        print('Please enter your name.')
+        name = input()
+        if name !='':
+            print('Thank you, ' + name + '.')
+            print('Enjoy your flight.')
