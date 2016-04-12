8908fba286c7bebd171eccaa8677b9ba6a1c92c6
Ryan <ryanblahnik@gmail.com>
2016-04-11 19:37:15 -0500

    Added files via upload
    009 + 010

---
+++ for 2.py
@@ -0,0 +1,4 @@
+total = 0
+for num in range(1001):
+    total = total + num
+print (total)
---
+++ for.py
@@ -0,0 +1,6 @@
+print('My name is')
+for i in range (20, -10, -2):
+    print('Jimmy Five Times ' + str(i))
+
+    
+    
