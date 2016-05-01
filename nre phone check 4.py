14fad49dba85269dbf7dd17c5a003f00f11c6808
Ryan <ryanblahnik@gmail.com>
2016-05-01 07:56:40 -0500

    Added files via upload
    034

---
+++ nre phone check 4.py
@@ -0,0 +1,11 @@
+import re
+
+print('Entry:')
+
+entry = input()
+
+phone_num_regex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+
+match_object = phone_num_regex.search(entry)
+
+print(match_object.group())
